44d24692bdf8cae0e5b9cc04263dbd853ce7cdf1
GauravXd <grvdhingra1999@gmail.com>
2020-04-14 04:59:49 +0530

    Local site scraping

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 
 async function main() {
-    const html = await request.get("https://reactnativetutorial.net/css-selectors/");
-    fs.writeFileSync("./test.html",html);
+    const html = await request.get("https://reactnativetutorial.net/css-selectors/lesson2.html");
+    fs.writeFileSync("./test.html", html);
+
+    const $ = await cheerio.load(html);
+
+    // const theText = $("h1").text();
+    // console.log(theText);
+
+    // $("h2").each((index, element) => {
+    //     console.log($(element).text());
+    // })
+
+    // const theText = $("#red").text();
+    // console.log(theText);
+
+    // $('[data-customer="22293"]').text()
+    // $('[data-customer]').text()
 }
 
 main();
